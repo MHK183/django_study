abe6b2b805fc3b65ac7e39d37d5aa53bf1ebb477
Kim Min Hyeok <4438mh@naver.com>
2023-09-07 18:14:56 +0900

    장고 2일차

---
+++ pystagram/posts/admin.py
@@ -0,0 +1,55 @@
+from django.db.models import ManyToManyField
+from django.forms import CheckboxSelectMultiple
+from django.contrib import admin
+from posts.models import Post, PostImage, Comment, HashTag
+
+import admin_thumbnails
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+@admin_thumbnails.thumbnail("photo")
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "content",
+    ]
+    inlines = [
+        CommentInline,
+        PostImageInline
+    ]
+    formfield_overrides = {
+        ManyToManyField: {"widget": CheckboxSelectMultiple}
+    }
+
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "photo"
+    ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "content"
+    ]
+
+
+@admin.register(HashTag)
+class HashTagAdmin(admin.ModelAdmin):
+    pass
---
+++ pystagram/posts/templatetags/custom_tags.py
@@ -0,0 +1,9 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def concat(value, arg):
+    return f"{value}{arg}"
